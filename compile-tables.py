c1bf21f327667e98dbb0588bcb67a25ab6e194ac
Jonathan Campbell <jonathan@castus.tv>
2022-02-07 14:06:47 -0800

    Compile table base scan

--- compile-tables.py
+++ compile-tables.py
@@ -3,9 +3,48 @@
 import glob
 import json
 
+# dictionary: table id -> table object
+tables_by_id = { }
+
+# table object
+class Table:
+    id = None
+    name = None
+    base_json = None
+    description = None
+    notes = [ ]
+    def __init__(self,json):
+        self.base_json = json
+        if "id" in json:
+            self.id = json["id"]
+        if "table" in json:
+            self.name = json["table"]
+        if "description" in json:
+            self.description = json["description"]
+        if "notes" in json:
+            if isinstance(json["notes"], str):
+                self.notes.append(json["notes"])
+            elif isinstance(json["notes"], list):
+                self.notes.extend(json["notes"])
+            else:
+                raise Exception("Notes element is neither string nor array")
+
 def load_json(path):
     f = open(path,"r",encoding='utf-8')
     j = json.load(f)
     f.close()
     return j
- 
+
+def load_tables_base():
+    g = glob.glob("tables/**/*--base.json",recursive=True)
+    for path in g:
+        json = load_json(path)
+        if "table" in json:
+            table = Table(json)
+            if not table.id == None:
+                if table.id in tables_by_id:
+                    raise Exception("Table "+table.id+" already defined")
+                tables_by_id[table.id] = table
+
+load_tables_base()
+
