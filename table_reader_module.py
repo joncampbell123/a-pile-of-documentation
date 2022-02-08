b8f239b5fb09f9c1015a79a3b270cde2ced1bdc8
Jonathan Campbell <jonathan@castus.tv>
2022-02-07 18:55:39 -0800

    move out

---
+++ table_reader_module.py
@@ -0,0 +1,240 @@
+
+import os
+import glob
+import json
+import pathlib
+
+import common_json_help_module
+import book_reader_module
+
+# dictionary: table id -> table object
+tables_by_id = { }
+
+# table object
+class Table:
+    id = None
+    name = None
+    base_json = None
+    description = None
+    notes = [ ]
+    table = { }
+    columns = [ ]
+    sources = [ ]
+    key_column = None
+    table_format_type = None
+    def expand_source_book(self,src,bookid):
+        if not bookid in book_reader_module.book_by_id:
+            raise Exception("No such book "+bookid)
+        book = book_reader_module.book_by_id[bookid]
+        #
+        citation = { }
+        if not book.title == None:
+            citation["title"] = book.title
+        if not book.author == None:
+            citation["author"] = book.author
+        if not book.publisher == None:
+            citation["publisher"] = book.publisher
+        if not book.copyright_year == None:
+            citation["year"] = book.copyright_year
+        if not book.type == None:
+            citation["type"] = book.type
+        src["citation"] = citation
+        #
+        searchobj = None
+        hierarchy_key = None
+        match = [ ]
+        matches = 0;
+        matchname = [ ]
+        matchnamep = [ ]
+        matchobj = [ ]
+        for h in book.hierarchy:
+            # plural to singular. The book says "parts", "sections", the reference says "part", "section"
+            k = h
+            if k[-1:] == 's':
+                k = k[:-1]
+            # match
+            m = None
+            if k in src:
+                matches = matches + 1
+                m = src[k]
+            match.append(m);
+            matchname.append(k)
+            matchnamep.append(h)
+            matchobj.append(None)
+        if matches == 0:
+            return
+        # fill in the gaps by searching upward
+        i = len(match) - 1
+        valid_len = None
+        while i >= 0:
+            if match[i] == None:
+                i = i - 1
+                continue
+            #
+            if valid_len == None:
+                valid_len = i + 1
+            # look up the parent this came from
+            search = book.hierarchy_search.get(matchnamep[i])
+            if search == None:
+                raise Exception("Book hiearchy missing search map for "+matchnamep[i])
+            #
+            obj = None
+            for tobj in search:
+                if tobj["name lookup"] == match[i]:
+                    obj = tobj;
+                    break
+            if obj == None:
+                raise Exception("No such "+matchname[i]+" named "+match[i])
+            matchobj[i] = obj
+            if i < 1:
+                break
+            pobj = obj.get("parent lookup")
+            if pobj == None:
+                raise Exception("No parent for "+matchname[i]+" named "+match[i])
+            if not pobj["type"] in book.hierarchy_search:
+                raise Exception("Book hiearchy missing search map for "+pobj["type"])
+            i = i - 1
+            if not pobj["type"] == matchnamep[i]:
+                raise Exception("Wrong parent type. Wanted "+pobj["type"]+" got "+matchnamep[i])
+            if not match[i] == None:
+                if not pobj["name"] == match[i]:
+                    raise Exception("Wrong parent name. Wanted "+pobj["name"]+" got "+match[i])
+            else:
+                match[i] = pobj["name"]
+        #
+        if valid_len == None:
+            valid_len = 0
+        while len(match) > valid_len:
+            match.pop()
+        while len(matchname) > valid_len:
+            matchname.pop()
+        while len(matchnamep) > valid_len:
+            matchnamep.pop()
+        #
+        src["where"] = [ ]
+        for i in range(valid_len):
+            if matchname[i] in src:
+                del src[matchname[i]]
+            #
+            obj = { }
+            if not match[i] == None:
+                obj["path"] = match[i]
+            if not matchname[i] == None:
+                obj["type"] = matchname[i]
+            if not matchobj[i] == None:
+                if isinstance(matchobj[i],dict):
+                    if "title" in matchobj[i]:
+                        obj["title"] = matchobj[i]["title"]
+                    if "page" in matchobj[i]:
+                        obj["page"] = matchobj[i]["page"]
+            src["where"].append(obj)
+    def expand_source(self):
+        for src in self.sources:
+            if "book" in src:
+                self.expand_source_book(src,src["book"])
+    def serialize_to_compiled_json(self):
+        f = { }
+        f["table"] = self.table
+        f["sources"] = self.sources
+        f["description"] = self.description
+        f["notes"] = self.notes
+        f["name"] = self.name
+        f["columns"] = self.columns
+        f["table format type"] = self.table_format_type
+        if not self.key_column == None:
+            f["key column"] = self.key_column
+        return f;
+    def filter_key_value_by_type(self,key):
+        if not self.key_column == None:
+            type = self.key_column.get("type")
+            if type == "uint8_t" or type == "uint16_t" or type == "uint32_t" or type == "uint":
+                return hex(int(key,0))
+        #
+        return key
+    def add_info(self,json):
+        source_idx = None
+        if "source" in json:
+            source_idx = len(self.sources)
+            json["source"]["source index"] = source_idx
+            self.sources.append(json["source"])
+        if "table" in json:
+            s_table = json["table"]
+            if isinstance(s_table, dict):
+                keys = s_table.keys()
+                for key in keys:
+                    if self.table_format_type == "key=value":
+                        key_str = key
+                        table_key = self.filter_key_value_by_type(key)
+                        if not table_key in self.table:
+                            self.table[table_key] = [ ]
+                        if not source_idx == None:
+                            s_table[key]["source index"] = source_idx
+                        s_table[key]["original key"] = key_str
+                        self.table[table_key].append(s_table[key])
+    def __init__(self,json):
+        self.base_json = json
+        if "id" in json:
+            self.id = json["id"]
+        if "table" in json:
+            self.name = json["table"]
+        if "description" in json:
+            self.description = json["description"]
+        if "notes" in json:
+            if isinstance(json["notes"], str):
+                self.notes.append(json["notes"])
+            elif isinstance(json["notes"], list):
+                self.notes.extend(json["notes"])
+            else:
+                raise Exception("Notes element is neither string nor array in "+self.id)
+        #
+        ok = False
+        if "base definition" in json:
+            if json["base definition"] == True:
+                ok = True
+        if not ok == True:
+            raise Exception("Table json is not base definition in "+self.id)
+        #--NTS: We'll support non-combineable later
+        ok = False
+        if "combinable" in json:
+            if json["combinable"] == True:
+                ok = True
+        if not ok == True:
+            raise Exception("Table json is not combinable in "+self.id)
+        #
+        if "table format" in json:
+            obj = json["table format"]
+            self.table_format_type = obj.get("type")
+            if self.table_format_type == "key=value":
+                self.key_column = obj["key"] # will throw exception if these keys do not exist
+                self.columns = [ obj["value"] ] # will throw exception if these keys do not exist
+            else:
+                raise Exception("Table json unknown table format type "+str(self.table_format_type)+" in "+self.id)
+
+def load_tables_base():
+    g = glob.glob("tables/**/*--base.json",recursive=True)
+    for path in g:
+        json = common_json_help_module.load_json(path)
+        if "table" in json:
+            table = Table(json)
+            if not table.id == None:
+                if table.id in tables_by_id:
+                    raise Exception("Table "+table.id+" already defined")
+                tables_by_id[table.id] = table
+
+def load_tables():
+    g = glob.glob("tables/**/*.json",recursive=True)
+    for path in g:
+        table_id = None
+        json = common_json_help_module.load_json(path)
+        if "base definition" in json:
+            if json["base definition"] == True:
+                continue
+        if "id" in json:
+            table_id = json["id"]
+        #
+        if table_id in tables_by_id:
+            table = tables_by_id[table_id]
+            table.add_info(json)
+        else:
+            raise Exception("Table json refers to undefined table id "+str(table_id)+" in json file "+path)
+
