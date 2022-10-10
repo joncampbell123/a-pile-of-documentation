7a51126c7e00315364d522050cd396bb273737d2
Jonathan Campbell <jonathan@castus.tv>
2022-10-09 23:08:47 -0700

    map

--- compile-sources.py
+++ compile-sources.py
@@ -23,6 +23,36 @@ def write_json(path,ji):
 # init
 books = { }
 
+def hierarchy_map_gen(hl,ji,sji,el):
+    if len(hl) == 0:
+        return
+    if not "by hierarchy" in ji:
+        ji["by hierarchy"] = { }
+    level = hl[0]
+    n_hl = hl[1:]
+    bs = ji["by hierarchy"]
+    if not level in bs:
+        bs[level] = { }
+    bse = bs[level]
+    if not level in sji:
+        return
+    eji = sji[level]
+    for ent in eji:
+        nel = el.copy()
+        nel.append(ent)
+        sent = eji[ent]
+        bse[ent] = nel
+        hierarchy_map_gen(n_hl,ji,sent,nel)
+
+def hierarchy_map(ji):
+    if not "hierarchy" in ji:
+        return
+    hl = ji["hierarchy"]
+    if not type(hl) == list:
+        return
+    hierarchy_map_gen(hl,ji,ji,[])
+
+# process
 g = glob.glob("sources/*.json",recursive=True)
 for path in g:
     ji = load_json(path)
@@ -32,7 +62,11 @@ for path in g:
         continue
     if ji["id"] in books:
         raise Exception("Book "+ji["id"]+" already exists")
+    #
     ji["source json file"] = path
+    #
+    hierarchy_map(ji)
+    #
     books[ji["id"]] = ji
 
 # write it
