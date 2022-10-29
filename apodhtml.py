9c17fb57a4d6529946958ea5ea8da4ecbee6eacf
Jonathan Campbell <jonathan@castus.tv>
2022-10-28 22:33:00 -0700

    more

--- apodhtml.py
+++ apodhtml.py
@@ -2,9 +2,9 @@
 def htmlidfilter(x):
     return re.sub('[^a-zA-Z0-9_\-\.]','_',x)
 
-# ex: <a id="id:source:part:part">...<a href="#id_source:part:part">duh</a>
-def mkhtmlid(sid,path=None):
-    r = "id:"+sid
+# ex: <a id="t:source:id:sourceid:part:partid">...<a href="#t:source:id:sourceid:part:partid">duh</a>
+def mkhtmlid(idtype,sid,path=None):
+    r = "t:"+idtype+":id:"+sid
     if not path == None:
         for pelo in path:
             if "level" in pelo and "name" in pelo:
