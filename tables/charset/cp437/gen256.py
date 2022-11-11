16f19f4bd2d8543509622b5563c3d5689b155967
Jonathan Campbell <jonathan@castus.tv>
2022-11-11 13:50:49 -0800

    more

--- tables/charset/cp437/gen256.py
+++ tables/charset/cp437/gen256.py
@@ -2,7 +2,45 @@
 
 # bootstrap for a simple 256-char 8-bit encoding
 
-print("charcode,character,comments")
+asciinames = [
+"NULL",
+"START OF HEADING",
+"START OF TEXT",
+"END OF TEXT",
+"END OF TRANSMISSION",
+"ENQUIRY",
+"ACKNOWLEDGE",
+"BELL",
+"BACKSPACE",
+"HORIZONTAL TABULATION",
+"LINE FEED",
+"VERTICAL TABULATION",
+"FORM FEED",
+"CARRIAGE RETURN",
+"SHIFT OUT",
+"SHIFT IN",
+"DATA LINK ESCAPE",
+"DEVICE CONTROL ONE",
+"DEVICE CONTROL TWO",
+"DEVICE CONTROL THREE",
+"DEVICE CONTROL FOUR",
+"NEGATIVE ACKNOWLEDGE",
+"SYNCHRONOUS IDLE",
+"END OF TRANSMISSION BLOCK",
+"CANCEL",
+"END OF MEDIUM",
+"SUBSTITUTE",
+"ESCAPE",
+"FILE SEPARATOR",
+"GROUP SEPARATOR",
+"RECORD SEPARATOR",
+"UNIT SEPARATOR"
+]
+
+print("charcode,name,character,comments")
 for i in range(0,256):
-    print(hex(i)+",,")
+    charname = ""
+    if i < 32:
+        charname = asciinames[i]
+    print(hex(i)+","+charname+",,")
 
