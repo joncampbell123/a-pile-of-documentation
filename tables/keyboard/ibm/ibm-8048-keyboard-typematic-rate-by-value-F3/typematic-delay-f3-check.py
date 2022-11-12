793ef5acb5bfbc7cbf1e5aef7fb82b952e7d7669
Jonathan Campbell <jonathan@castus.tv>
2022-11-11 18:54:16 -0800

    Programmatically generate the typematic table and then update it

---
+++ tables/keyboard/ibm/ibm-8048-keyboard-typematic-rate-by-value-F3/typematic-delay-f3-check.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# validate "ibm-8048-keyboard-typematic-rate-by-value-F3"
+print("value,rate,period")
+for val in range(0,32):
+    B = (val >> 3) & 3
+    A = val & 7
+    # 2 to the power of B is another way of saying 1 << B
+    # 2**0 == 1        1 << 0 == 1
+    # 2**1 == 2        1 << 1 == 2
+    # 2**2 == 4        1 << 2 == 4
+    # and so on
+    period = (8 + A) * (1 << B) * (1.0 / (30.0 * 8.0))
+    rate = 1.0 / period
+    print(str(val)+","+("%.2f" % rate)+","+("%.2f" % (period * 1000.0)))
