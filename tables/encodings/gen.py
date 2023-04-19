f19047900d0130c32335cd850e45efa451bac038
Jonathan Campbell <jonathan@castus.tv>
2023-04-18 17:54:38 -0700

    more

--- tables/encodings/gen.py
+++ tables/encodings/gen.py
@@ -374,3 +374,28 @@ for enti in sorto:
     csw.writerow([vhex,vdec,voct,vbin,unicp_s,ent.name,'',disp_s])
 f.close()
 
+#--------------------------------------------------------------------------------------------------------
+# list of numbers in various common bases
+# hexadecimal, decimal, octal, binary
+# NTS: The unicode listing does not list it, but code 0xF0 is a solid Apple logo. Which has no Unicode
+#      equivalent and will therefore need the image rendering mode for "display" column.
+map_current = load_unicode_mapping_file("ref/MAC-ROMAN.TXT")
+patch_cp437_control_codes(map_current)
+f = open("gen-apple-mac-roman.csv",mode="w",encoding="utf-8",newline="")
+csw = csv.writer(f)
+csw.writerow(['Code (hexadecimal)',      'Code (decimal)',          'Code (octal)',            'Code (binary)',          'Unicode code point','name',  'description','display',         '#column-names'])
+csw.writerow(['numeric:base=16,multiple','numeric:base=10,multiple','numeric:base=10,multiple','numeric:base=2,multiple','numeric:base=16',   'string','string',     'string/image',    '#column-format'])
+csw.writerow(['right',                   'right',                   'right',                   'right',                  'right',             'left',  'left',       'left',            '#column-align'])
+csw.writerow(['Apple Macintosh MacRoman table', '#table-title'])
+csw.writerow([])
+for enti in map_current:
+    ent = map_current[enti]
+    vhex = ent.getHexString()
+    vdec = ent.getDecString()
+    voct = ent.getOctString()
+    vbin = ent.getBinString()
+    unicp_s = ent.getUnicpString()
+    disp_s = ent.getDisplayString()
+    csw.writerow([vhex,vdec,voct,vbin,unicp_s,ent.name,'',disp_s])
+f.close()
+
