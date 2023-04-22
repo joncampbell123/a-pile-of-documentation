4808368d162b674d970606c1077a48434a31e278
Jonathan Campbell <jonathan@castus.tv>
2023-04-21 18:09:05 -0700

    Windows fonts, too

--- images/encodings/gen.py
+++ images/encodings/gen.py
@@ -388,7 +388,7 @@ def drawchargrid(*,imgt8=None,tcWidth=None,tcHeight=None,colDigits=2,imgcp,charC
         for ci in range(0,colDigits):
             dx = tcWidth*ci
             sx,sy = textMapFunc(ord(code[ci]))
-            imgmonocopy(img,dx,dy,8,16,imgt8,sx,sy,lambda _x: (_x ^ 1))
+            imgmonocopy(img,dx,dy,tcWidth,tcHeight,imgt8,sx,sy,lambda _x: (_x ^ 1))
     #
     for r in range(0,charRows):
         for c in range(0,charCols):
@@ -431,6 +431,147 @@ docWriteBMP("gen-cp864.bmp",drawchargrid(imgcp=docLoadBMP("ref/cp864vga8x16.bmp"
 
 #-----------------------------------------------------
 docWriteBMP("gen-cp866.bmp",drawchargrid(imgcp=docLoadBMP("ref/cp866vga8x16.bmp")))
+
+#-----------------------------------------------------
+pc98rom = None
+PC98FONT = None
+img_pc98sbcs = None
+img_pc98dbcs = None
+
+class MSWINFNT:
+    class charReturn:
+        bmp = None
+        width = None
+        height = None
+        stride = None
+    class headerStruct:
+        class charTableEntry:
+            charWidth = None
+            charOffset = None
+        # The documentation here is junk [https://jeffpar.github.io/kbarchive/kb/065/Q65123/]
+        # It does not list offsets, it fails to list sizes for some fields, and it lists fields
+        # without mentioning which version they belong to without mentioning that nothing exists
+        # past dfReserved for version 0x200 (the dfCharTable follows immediately after that).
+        fmt = "<HL60sHHHHHHHBBBHBHHBHHBBBBHLLLLB"
+        fmtsz = struct.calcsize(fmt)
+        dfVersion = None
+        dfSize = None
+        dfCopyright = None
+        dfType = None
+        dfPoints = None
+        dfVertRes = None
+        dfHorizRes = None
+        dfAscent = None
+        dfInternalLeading = None
+        dfExternalLeading = None
+        dfItalic = None
+        dfUnderline = None
+        dfStrikeOut = None
+        dfWeight = None
+        dfCharSet = None
+        dfPixWidth = None
+        dfPixHeight = None
+        dfPitchAndFamily = None
+        dfAvgWidth = None
+        dfMaxWidth = None
+        dfFirstChar = None
+        dfLastChar = None
+        dfDefaultChar = None
+        dfBreakChar = None
+        dfWidthBytes = None
+        dfDevice = None
+        dfFace = None
+        dfBitsPointer = None
+        dfBitsOffset = None
+        dfReserved = None
+        dfCharTableOffset = None
+        dfCharTable = None
+        #
+        def parse(self,raw):
+            self.dfVersion,self.dfSize,self.dfCopyright,self.dfType,self.dfPoints,self.dfVertRes,self.dfHorizRes,self.dfAscent,self.dfInternalLeading,self.dfExternalLeading,self.dfItalic,self.dfUnderline,self.dfStrikeOut,self.dfWeight,self.dfCharSet,self.dfPixWidth,self.dfPixHeight,self.dfPitchAndFamily,self.dfAvgWidth,self.dfMaxWidth,self.dfFirstChar,self.dfLastChar,self.dfDefaultChar,self.dfBreakChar,self.dfWidthBytes,self.dfDevice,self.dfFace,self.dfBitsPointer,self.dfBitsOffset,self.dfReserved = struct.unpack(self.fmt,raw[0:self.fmtsz])
+            if self.dfVersion < 0x200 or self.dfVersion > 0x3FF or self.dfSize > len(raw) or self.dfFirstChar > self.dfLastChar:
+                raise Exception("windows fnt header error")
+            # Fuck, figure out the offset despite the mediocre documentation URL listed above
+            if self.dfVersion >= 0x300:
+                self.dfCharTableOffset = 0x94; # FIXME: Untested
+            else:
+                self.dfCharTableOffset = 0x76;
+            #
+            charTableEntries = self.dfLastChar + 2 - self.dfFirstChar
+            self.dfCharTable = [ None ] * charTableEntries
+            if self.dfVersion >= 0x300:
+                for i in range(0,charTableEntries):
+                    ent = self.charTableEntry()
+                    blobofs = self.dfCharTableOffset+(i*4)
+                    blob = raw[blobofs:blobofs+4]
+                    ent.charWidth,ent.charOffset = struct.unpack("<HL",blob)
+                    self.dfCharTable[i] = ent
+            else:
+                for i in range(0,charTableEntries):
+                    ent = self.charTableEntry()
+                    blobofs = self.dfCharTableOffset+(i*4)
+                    blob = raw[blobofs:blobofs+4]
+                    ent.charWidth,ent.charOffset = struct.unpack("<HH",blob)
+                    self.dfCharTable[i] = ent
+        #
+        def __str__(self):
+            return "[MSWINFNT dfVersion="+hex(self.dfVersion)+" dfCopyright="+str(self.dfCopyright)+" dfType="+str(self.dfType)+" dfPoints="+str(self.dfPoints)+" dfAscent="+str(self.dfAscent)+" dfInternalLeading="+str(self.dfInternalLeading)+" dfExternalLeading="+str(self.dfExternalLeading)+" dfCharSet="+str(self.dfCharSet)+" dfPixWidth="+str(self.dfPixWidth)+" dfPixHeight="+str(self.dfPixHeight)+" dfPitchAndFamily="+hex(self.dfPitchAndFamily)+" dfMaxWidth="+str(self.dfMaxWidth)+" dfFirstChar="+str(self.dfFirstChar)+" dfLastChar="+str(self.dfLastChar)+" dfDefaultChar="+str(self.dfDefaultChar)+" dfWidthBytes="+str(self.dfWidthBytes)+" dfBitsOffset="+str(self.dfBitsOffset)+" dfCharTableOffset="+str(self.dfCharTableOffset)+"]"
+        def __init__(self,raw):
+            self.parse(raw)
+    #
+    fntraw = None
+    header = None
+    def parse(self,fntraw):
+        self.header = self.headerStruct(fntraw)
+    def __init__(self,path):
+        f = open(path,mode="rb")
+        self.fntraw = f.read()
+        f.close()
+        self.parse(self.fntraw)
+    def getchar(self,c,*,genDocImage=False):
+        if type(c) == str:
+            if len(c) > 1:
+                raise Exception("Only one char allowed")
+            c = ord(c[0])
+        #
+        idx = self.header.dfDefaultChar
+        if c >= self.header.dfFirstChar and c <= self.header.dfLastChar:
+            idx = c - self.header.dfFirstChar
+        #
+        r = self.charReturn()
+        sent = self.header.dfCharTable[idx]
+        r.width = sent.charWidth
+        r.height = self.header.dfPixHeight
+        r.stride = int((r.width+7)/8)
+        bmpo = sent.charOffset
+        r.bmp = self.fntraw[bmpo:bmpo+(r.height*r.stride)]
+        #
+        if genDocImage == True:
+            # OK, so get this: Microsoft raster fonts are stored as vertical strips. They are not bitmaps like you are accustomed to.
+            # That means for a 9-pixel wide "B" for example, the left 8 pixels are sent, then the right 8 pixels.
+            r.docImage = docImage(r.width,r.height,1)
+            for col in range(0,r.stride):
+                for row in range(0,r.height):
+                    r.docImage.rows[row][col] = r.bmp[(col*r.height)+row]
+        #
+        return r
+
+# Windows code page 1252, System font extracted from Windows 3.1
+win31sysfnt = MSWINFNT("ref/window31_system_font_cp1252.fnt")
+wgridw = win31sysfnt.header.dfMaxWidth
+wgridh = win31sysfnt.header.dfPixHeight
+wgrid = docImage(wgridw*16,wgridh*16,1)
+wgrid.fillrect(0,0,wgrid.width,wgrid.height,0)
+for r in range(0,16):
+    for c in range(0,16):
+        cc = (r << 4) + c
+        wbmpinfo = win31sysfnt.getchar(cc,genDocImage=True)
+        imgmonocopy(wgrid,c*wgridw,r*wgridh,wbmpinfo.width,wbmpinfo.height,wbmpinfo.docImage,0,0,lambda _x: _x)
+#
+docWriteBMP("gen-windows31-cp1252-system.bmp",drawchargrid(imgcp=wgrid,charCellWidth=wgridw,charCellHeight=wgridh))
+win31sysfnt = None
+wbmpinfo = None
+wgrid = None
 
 #-----------------------------------------------------
 # PC-98 FONT ROM, video memory text codes
@@ -578,10 +719,4 @@ for hib in range(0x20,0x80,0x04):
     docWriteBMP("gen-pc98-tvram-"+sc+".bmp",docImageStackCombine(imgs))
     #
     imgs = None
-
-#-----------------------------------------------------
-pc98rom = None
-PC98FONT = None
-img_pc98sbcs = None
-img_pc98dbcs = None
 
