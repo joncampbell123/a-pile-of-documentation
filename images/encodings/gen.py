07e1974d04cd42f02e9bffb5f342980eca05343c
Jonathan Campbell <jonathan@castus.tv>
2024-05-19 14:56:27 -0700

    correct dosbox font dump, add win fnt dump, conditional

--- images/encodings/gen.py
+++ images/encodings/gen.py
@@ -86,7 +86,7 @@ def drawchargrid(*,imgt8=None,tcWidth=None,tcHeight=None,colDigits=2,imgcp,charC
 #-----------------------------------------------------
 def do_dosbox_fontromdump(source,dest,w=8,h=16):
     if is_newer_than(source=source,dest=dest):
-        docWriteBMP(dest,drawchargrid(imgcp=docLoadBMP(source)),charCellWidth=w,charCellHeight=h)
+        docWriteBMP(dest,drawchargrid(imgcp=docLoadBMP(source),charCellWidth=w,charCellHeight=h))
 
 do_dosbox_fontromdump(source="ref/cp437vga8x16.bmp",dest="gen-cp437.bmp")
 do_dosbox_fontromdump(source="ref/cp850vga8x16.bmp",dest="gen-cp850.bmp")
@@ -127,11 +127,15 @@ def loadAndRenderWindowsFNT(*,path=None,raw=None):
 
 #-----------------------------------------------------
 # Windows FNT font files
-docWriteBMP("gen-windows31-cp1252-system.bmp",loadAndRenderWindowsFNT(raw=WindowsNEResourceReader(path="ref/windows31_system.fon").getResource(resTypeID=8,resID=0x1F).data))
-docWriteBMP("gen-windows31-cp1252-fixedsys.bmp",loadAndRenderWindowsFNT(raw=WindowsNEResourceReader(path="ref/windows31_fixedsys.fon").getResource(resTypeID=8,resID=0x1F).data))
-docWriteBMP("gen-windows31-cp1252-sans-serif-8pt.bmp",loadAndRenderWindowsFNT(raw=WindowsNEResourceReader(path="ref/windows31_sans_serif.fon").getResource(resTypeID=8,resID=0x54).data))
-docWriteBMP("gen-windows31-symbole.bmp",loadAndRenderWindowsFNT(raw=WindowsNEResourceReader(path="ref/windows31_symbole.fon").getResource(resTypeID=8,resID=6).data))
-docWriteBMP("gen-windows31-smalle.bmp",loadAndRenderWindowsFNT(raw=WindowsNEResourceReader(path="ref/windows31_smalle.fon").getResource(resTypeID=8,resID=8).data))
+def do_win_ne_fnt_res(source,dest,resTypeID,resID):
+    if is_newer_than(source=source,dest=dest):
+        docWriteBMP(dest,loadAndRenderWindowsFNT(raw=WindowsNEResourceReader(path=source).getResource(resTypeID=resTypeID,resID=resID).data))
+
+do_win_ne_fnt_res(source="ref/windows31_system.fon",        dest="gen-windows31-cp1252-system.bmp",            resTypeID=8,resID=0x1F)
+do_win_ne_fnt_res(source="ref/windows31_fixedsys.fon",      dest="gen-windows31-cp1252-fixedsys.bmp",          resTypeID=8,resID=0x1F)
+do_win_ne_fnt_res(source="ref/windows31_sans_serif.fon",    dest="gen-windows31-cp1252-sans-serif-8pt.bmp",    resTypeID=8,resID=0x54)
+do_win_ne_fnt_res(source="ref/windows31_symbole.fon",       dest="gen-windows31-symbole.bmp",                  resTypeID=8,resID=0x06)
+do_win_ne_fnt_res(source="ref/windows31_smalle.fon",        dest="gen-windows31-smalle.bmp",                   resTypeID=8,resID=0x08)
 
 #-----------------------------------------------------
 PC98FONT = PC98FONTROM(path="ref/pc98font.rom")
