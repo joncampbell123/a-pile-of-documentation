24e32f5014a2ef8690c5fbd8ca54f85d291e001a
Jonathan Campbell <jonathan@castus.tv>
2025-06-01 03:14:37 -0700

    more

--- devtest/text/read.py
+++ devtest/text/read.py
@@ -10,7 +10,18 @@ def splitlines(blob):
     return re.split(b'\n\r|\r\n|\r|\n',blob)
 
 f = open(inFile,"rb")
-for line in splitlines(f.read()):
-    print(b"Line> \""+line+b"\" <eol>")
+rawtxt = f.read()
 f.close()
+
+print("-----RAW-----")
+for line in splitlines(rawtxt):
+    print(b"Line> \""+line+b"\" <eol>")
+
+print("-----UTF-8-----")
+for rawline in splitlines(rawtxt):
+    try:
+        line = rawline.decode('utf-8')
+        print("Line> \""+line+"\" <eol>")
+    except:
+        print("Line> (failed to decode)")
 
