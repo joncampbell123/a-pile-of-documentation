c3fa02576d63fe01a246051adcee45126fbd6633
Jonathan Campbell <jonathan@castus.tv>
2025-06-07 01:21:56 -0700

    more

---
+++ devtest/html/read.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import os
+import re
+import sys
+
+def rawhtmlloadfile(path):
+    f = open(path,"rb")
+    raw = f.read()
+    f.close()
+    return raw
+
+sys.path.append(os.path.join(os.path.dirname(__file__),'..','..'))
+
+inFile = sys.argv[1]
+rawhtml = rawhtmlloadfile(inFile)
+
