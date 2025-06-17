f91f4befb87c7fd3d4f58792d4b664b181711110
Jonathan Campbell <jonathan@castus.tv>
2025-06-16 17:30:10 -0700

    some parsing

--- devtest/html/readcss.py
+++ devtest/html/readcss.py
@@ -19,6 +19,78 @@ def rawcssloadfile(path):
     f.close()
     return raw
 
+class CSSllToken:
+    token = None
+    text = None
+    #
+    def __init__(self):
+        True
+    def __str__(self):
+        r = "[CSSllToken"
+        if not self.token == None:
+            r += " token="+str(self.token)
+        if not self.text == None:
+            r += " text="+str(self.text)
+        r += "]"
+        return r
+
+class CSSllState:
+    encoding = None
+
 inFile = sys.argv[1]
 rawcss = rawcssloadfile(inFile)
+
+def CSSskipwhitespace(blob,i):
+    r = re.search(r'[ \t\n\r\f]+',blob[i:])
+    if r:
+        return r.span()[1]+i
+    else:
+        return i
+
+def CSSllparse(blob,state=CSSllState()):
+    i = 0
+    # NTS: You should set encoding to the value given from the HTML parser, if possible
+    if state.encoding == None:
+        state.encoding = "utf-8"
+    #
+    blob = blob.decode(state.encoding)
+    #
+    while i < len(blob):
+        i = CSSskipwhitespace(blob,i)
+        if i >= len(blob):
+            break
+        #
+        if blob[i:i+2] == '/*':
+            begin = i+2
+            r = re.search(r'\*\/',blob[begin:])
+            if r:
+                end = r.span()[0]+begin
+                i = r.span()[1]+begin
+            else:
+                i = end = len(blob)
+            #
+            t = CSSllToken()
+            t.token = 'comment'
+            t.text = blob[begin:end]
+            yield t
+            continue
+        #
+        r = re.match(r'^((-{0,1}[a-zA-Z_\u0080-\uFFFFFF]|--[a-zA-Z0-9_\u0080-\uFFFFFF])[a-zA-Z0-9_\u0080-\uFFFFFF]*)',blob[i:])
+        if r:
+            begin = r.span()[0]+i
+            end = r.span()[1]+i
+            i = end
+            #
+            t = CSSllToken()
+            t.token = 'ident'
+            t.text = blob[begin:end]
+            yield t
+            continue
+        #
+        break
+
+state = CSSllState()
+
+for ent in CSSllparse(rawcss,state):
+    print(ent)
 
