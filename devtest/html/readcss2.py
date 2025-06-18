e1e2ad89ee27e4eeea08caac9faf9d5f99e65673
Jonathan Campbell <jonathan@castus.tv>
2025-06-18 09:24:59 -0700

    Fixup, excess whitespace elimination, mid level convert . identifier into class token

--- devtest/html/readcss2.py
+++ devtest/html/readcss2.py
@@ -14,6 +14,8 @@ class CSSmidState:
     llstate = None
     lookahead = None
     comments = None
+    prev = None # warning: prev token returned by get()
+    last = None # warning: last token returned by get()
     def __init__(self):
         self.comments = [ ]
         self.lookahead = [ ]
@@ -27,6 +29,15 @@ class CSSmidState:
                 t = next(self.bliter)
                 if t.token == 'comment':
                     self.comments.append(t)
+                elif t.token == 'ws': # append the 'ws' token, then pull additional tokens and loop while they are 'ws'
+                    self.lookahead.append(t)
+                    while True:
+                        t = next(self.bliter)
+                        if t.token == 'comment': # skip comments as we go
+                            continue
+                        elif not t.token == 'ws': # if not 'ws' then stop, else just ignore it
+                            self.lookahead.append(t)
+                            break
                 else:
                     self.lookahead.append(t)
             except StopIteration:
@@ -34,16 +45,17 @@ class CSSmidState:
         #
         return self.lookahead[i]
     def get(self):
-        r = self.peek()
+        self.prev = self.last
+        self.last = r = self.peek()
         self.discard()
         return r
-    def discard(self,i=1):
-        if i > 0:
-            i -= 1
-            self.peek(i)
-            if len(self.lookahead) < i:
-                raise Exception("OOPS")
-            self.lookahead = [ ]
+    def discard(self): # TODO: Add second parameter i if multiple token discard is ever needed
+        self.peek() # fill lookahead
+        #
+        if len(self.lookahead) < 1:
+            raise Exception("OOPS")
+        #
+        self.lookahead = self.lookahead[1:]
 
 state = CSSmidState()
 
@@ -54,6 +66,34 @@ def CSSmidparse(blob,state=CSSmidState()):
         t = state.get()
         if not t:
             break
+        # eat whitespace here unless the next token is '.' or '#' because
+        # whitespace matters to determine whether you're requiring them all
+        # to match i.e. .class1.class2 to require "class1 class2" or .class1 .class2
+        # that one should appear as a child to the other. Apparently .class1#id2
+        # is also valid to require class="class1" and id="id2".
+        #
+        # it also matters, the difference between: "p.class" and "p .class"
+        if t.token == 'ws':
+            #
+            tp = state.prev
+            if not tp == None:
+                if tp.token == 'char' and (tp.text == '{' or tp.text == '}' or tp.text == ':'): # if it follows these tokens there is no chance of chaining # and .
+                    continue # skip
+            #
+            tn = state.peek()
+            if tn.token == 'char' and tn.text == '.':
+                yield t
+            elif tn.token == 'hash':
+                yield t
+            continue
+        # .class?
+        if t.token == 'char' and t.text == '.':
+            t = state.get()
+            if not t:
+                break
+            t.token = 'class'
+            yield t
+            continue
         #
         yield t
 
