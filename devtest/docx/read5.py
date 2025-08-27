1bdb44fb6e7e40247d5c1c1d88bed2efe95ca99b
Jonathan Campbell <jonathan@castus.tv>
2025-08-27 15:31:48 -0700

    more

---
+++ devtest/docx/read5.py
@@ -0,0 +1,146 @@
+#!/usr/bin/python3
+
+import os
+import sys
+import zlib
+import struct
+
+sys.path.append(os.path.join(os.path.dirname(__file__),'..','..'))
+
+from apodlib.docZIPhigh import *
+
+inFile = sys.argv[1]
+
+from apodlib.docHTMLhi import *
+
+class OOXMLReader:
+    ok = False
+    zipreader = None
+    zipmetamap = None
+    #
+    class zipfilemeta:
+        contentType = None
+        def __str__(self):
+            r = "[zipfilemeta"
+            if not self.contentType == None:
+                r += " contentType="+str(self.contentType)
+            r += "]"
+            return r
+    #
+    def __init__(self,inFile):
+        self.zipmetamap = { }
+        self.zipreader = ZIPHighReader(inFile)
+        self.zipreader.scan()
+        #
+        def metainit(self,ppath="/"):
+            spath = ppath
+            if spath == "/":
+                spath = ""
+            #
+            for ent in self.zipreader.opendir(ppath):
+                if ent.dtype == 'dir':
+                    metainit(self,ppath=spath+"/"+ent.dname)
+                else:
+                    fpath = self.zipreader.normalizepath(spath+"/"+ent.dname)
+                    if not fpath in self.zipmetamap:
+                        self.zipmetamap[fpath] = OOXMLReader.zipfilemeta()
+        #
+        metainit(self)
+        #
+        self.parsecontenttype()
+    def close(self):
+        if self.zipreader:
+            self.zipreader.close()
+        self.zipreader = None
+    def registerZIPContentType(self,path,contenttype,*,policy=None): # WARNING: path is expected to contain normalized path, caller must do it
+        if policy == None:
+            policy = { }
+        if not "replace" in policy:
+            policy["replace"] = True
+        #
+        if not path in self.zipmetamap:
+            self.zipmetamap[path] = OOXMLReader.zipfilemeta()
+        #
+        zmm = self.zipmetamap[path]
+        #
+        if not policy["replace"] == True and not zmm.contentType == None:
+            return
+        #
+        if contenttype:
+            zmm.contentType = contenttype
+        else:
+            zmm.contentType = None
+    def registerZIPContentTypeByExtension(self,ext,contenttype,*,ppath="/"):
+        spath = ppath
+        if spath == "/":
+            spath = ""
+        #
+        for ent in self.zipreader.opendir(ppath):
+            if ent.dtype == 'dir':
+                self.registerZIPContentTypeByExtension(ext,contenttype,ppath=spath+"/"+ent.dname)
+            else:
+                f_ext = None
+                chk = ent.dname.lower()
+                x = chk.rfind('.')
+                if x >= 0:
+                    f_ext = chk[x+1:]
+                if f_ext == ext:
+                    self.registerZIPContentType(self.zipreader.normalizepath(spath+"/"+ent.dname),contenttype,policy={ "replace": False })
+    # parse [Content_Types].xml
+    def parsecontenttype(self):
+        if self.zipreader == None:
+            return
+        zf = self.zipreader.open("/[Content_Types].xml")
+        if zf == None:
+            return
+        raw = zf.read()
+        xml = HTMLhiReaderState()
+        HTMLhiParseAll(raw,xml)
+        xroot = xml.getRoot()
+        xTypes = None
+        # look for Types
+        if xroot:
+            for tag in xroot.children:
+                if tag.elemType == 'tag':
+                    if tag.tag.lower() == 'types':
+                        xTypes = tag
+        # found Types, read the Override and Default tags
+        if xTypes:
+            for tag in xTypes.children:
+                if tag.elemType == 'tag':
+                    if tag.tag.lower() == 'override':
+                        PartName = None
+                        ContentType = None
+                        for attr in tag.attr:
+                            if attr.name.lower() == 'partname':
+                                PartName = self.zipreader.normalizepath(attr.value)
+                            elif attr.name.lower() == 'contenttype':
+                                ContentType = attr.value
+                        #
+                        if PartName and ContentType:
+                            self.registerZIPContentType(PartName,ContentType)
+                    #
+                    elif tag.tag.lower() == 'default':
+                        Extension = None
+                        ContentType = None
+                        for attr in tag.attr:
+                            if attr.name.lower() == 'extension':
+                                Extension = attr.value
+                            elif attr.name.lower() == 'contenttype':
+                                ContentType = attr.value
+                        #
+                        if Extension and ContentType:
+                            self.registerZIPContentTypeByExtension(Extension,ContentType)
+                    #
+        # done
+        del zf
+    def dbg_dump_metamap(self):
+        print("Debug dump zipmetamap")
+        for ent in self.zipmetamap:
+            print(str(ent)+": "+str(self.zipmetamap[ent]))
+        print("")
+
+zr = OOXMLReader(inFile)
+zr.dbg_dump_metamap()
+zr.close()
+
