544796f39faccf99c1a80c6debf78338c7fc9679
Jonathan Campbell <jonathan@castus.tv>
2025-08-28 09:33:48 -0700

    more

--- devtest/csv/read2.py
+++ devtest/csv/read2.py
@@ -6,25 +6,12 @@ import sys
 
 sys.path.append(os.path.join(os.path.dirname(__file__),'..','..'))
 
-from apodlib.docCSV import *
+from apodlib.docCSVmid import *
 
 inFile = sys.argv[1]
 rawcsv = rawcsvloadfile(inFile)
 
 csvr = CSVReaderState()
-
-def CSVmidParse(rawcsv,csvr):
-    row = [ ]
-    for csvrecord in CSVllParse(rawcsv,csvr):
-        if csvrecord == '\n':
-            yield row
-            row = [ ]
-        else:
-            row.append(csvrecord)
-    #
-    if len(row) > 0:
-        yield row
-        row = [ ]
 
 for csvrow in CSVmidParse(rawcsv,csvr):
     row = ""
